75ca3ed10f5a5c5692e0df57e78d1ea14c912ff7
Papoy <imaderexy@gmail.com>
2022-11-10 09:05:56 +0700

    GD10

---
+++ app/src/main/java/com/example/gd10_b_10864/RClient.kt
@@ -0,0 +1,16 @@
+package com.example.gd10_b_10864
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RClient {
+    private const val BASE_URL = "http://10.53.1.214/ci4-apiserver/public/"
+    val instances: api by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(api::class.java)
+    }
+}
